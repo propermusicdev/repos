118f13109066f4378aebff11d6817ecbca7a3b98
win7Dev <sam.rae@propermusicgroup.com>
2019-06-06 11:48:48 +0100

    20190606114846

---
+++ hash/hash/com/propermusicgroup/cs/hash/Encryption/TheCrypt.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.propermusicgroup.cs.hash.Encryption
+{
+  public static class TheCrypt
+  {
+
+    public static string GetSHA1Base64_string(string s)
+    {
+      return GetSHA1Base64(Encoding.UTF8.GetBytes(s));
+    }
+    public static string GetSHA1Base64(byte[] bytes)
+    {
+      return Base64BytesEncode(GetSHA1(bytes));
+    }
+
+    public static byte[] GetSHA1(byte[] bytes)
+    {
+
+      return GetHashBytes(SHA1.Create(), bytes);
+
+
+    }
+
+    public static string Base64BytesEncode(byte[] bytes)
+    {
+      return Convert.ToBase64String(bytes);
+    }
+
+
+    public static string Base64Encode(string plainText)
+    {
+      var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+      return Convert.ToBase64String(plainTextBytes);
+    }
+
+    public static string Base64Decode(string base64EncodedData)
+    {
+      var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
+      return Encoding.UTF8.GetString(base64EncodedBytes);
+    }
+    public static byte[] GetHashBytes(HashAlgorithm hashAlgorithm, byte[] input)
+    {
+      return hashAlgorithm.ComputeHash(input);
+    }
+
+    public static string GetHash(HashAlgorithm hashAlgorithm, string input)
+    {
+
+      // Convert the input string to a byte array and compute the hash.
+      byte[] data = hashAlgorithm.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+      // Create a new Stringbuilder to collect the bytes
+      // and create a string.
+      var sBuilder = new StringBuilder();
+
+      // Loop through each byte of the hashed data 
+      // and format each one as a hexadecimal string.
+      for (int i = 0; i < data.Length; i++)
+      {
+        sBuilder.Append(data[i].ToString("x2"));
+      }
+
+      // Return the hexadecimal string.
+      return sBuilder.ToString();
+    }
+  }
+}
---
+++ hash/hash/com/propermusicgroup/cs/hash/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using com.propermusicgroup.cs.hash.Encryption;
+
+
+namespace com.propermusicgroup.cs.hash
+{
+  class Program
+  {
+    private static string createdTimestamp;
+    private static string password;
+    private static string outputPath;
+    private static string nonce;
+    static void Main(string[] args)
+    {
+      if (args.Length == 3)
+      {
+        char c = (char)new Random().Next((int)'0', (int)'9');
+        nonce = Guid.NewGuid().ToString().Replace('-', c);
+        nonce = nonce.Substring(0, 16);
+        //Random random = new Random();
+        //for (int i = 0; i < 4; i += 1)
+        //{
+        //  nonce += random.Next(1000, 9999).ToString();
+        //}
+        Console.WriteLine(nonce);
+        createdTimestamp = args[0];
+        password = args[1];
+        outputPath = args[2];
+
+        List<string> output = new List<string>();
+        string pwd = TheCrypt.GetSHA1Base64_string(nonce + createdTimestamp + TheCrypt.GetSHA1Base64_string(password));
+        Console.WriteLine(pwd + " length:" + pwd.Length);
+
+        output.Add("timestamp\t" + createdTimestamp);
+        output.Add("outputPath\t" + outputPath);
+        output.Add("base64Nonce\t" + TheCrypt.Base64Encode(nonce));
+          
+        output.Add("nonce\t" + nonce);
+        output.Add("digest\t" + pwd);
+        WriteToFile(output);
+        
+      }
+      //Console.ReadLine();
+
+      
+
+    }
+    public static void WriteToFile(List<string> lines)
+    {
+      File.WriteAllLines(outputPath, lines);
+
+  }
+
+  }
+
+
+}
