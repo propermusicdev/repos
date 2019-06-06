9f447f71230e7d006e2884ef8ad4e8baab318f46
win7Dev <sam.rae@propermusicgroup.com>
2019-06-06 13:29:11 +0100

    20190606132909

--- hash/hash/com/propermusicgroup/cs/hash/Program.cs
+++ hash/hash/com/propermusicgroup/cs/hash/Program.cs
@@ -18,24 +18,31 @@ namespace com.propermusicgroup.cs.hash
     private static string nonce;
     static void Main(string[] args)
     {
-      if (args.Length == 3)
+      if (args.Length >= 3)
       {
-        char c = (char)new Random().Next((int)'0', (int)'9');
-        nonce = Guid.NewGuid().ToString().Replace('-', c);
-        nonce = nonce.Substring(0, 16);
+        if (args.Length == 4)
+        {
+          nonce = args[3];
+        } else
+        {
+          char c = (char)new Random().Next((int)'0', (int)'9');
+          nonce = Guid.NewGuid().ToString().Replace('-', c);
+          nonce = nonce.Substring(0, 16);
+        }
+
         //Random random = new Random();
         //for (int i = 0; i < 4; i += 1)
         //{
         //  nonce += random.Next(1000, 9999).ToString();
         //}
-        Console.WriteLine(nonce);
+        //Console.WriteLine(nonce);
         createdTimestamp = args[0];
         password = args[1];
         outputPath = args[2];
 
         List<string> output = new List<string>();
         string pwd = TheCrypt.GetSHA1Base64_string(nonce + createdTimestamp + TheCrypt.GetSHA1Base64_string(password));
-        Console.WriteLine(pwd + " length:" + pwd.Length);
+        //Console.WriteLine(pwd + " length:" + pwd.Length);
 
         output.Add("timestamp\t" + createdTimestamp);
         output.Add("outputPath\t" + outputPath);
@@ -43,6 +50,10 @@ namespace com.propermusicgroup.cs.hash
           
         output.Add("nonce\t" + nonce);
         output.Add("digest\t" + pwd);
+        //foreach(string s in  output)
+        //{
+        //  Console.WriteLine(s);
+        //}
         WriteToFile(output);
         
       }
